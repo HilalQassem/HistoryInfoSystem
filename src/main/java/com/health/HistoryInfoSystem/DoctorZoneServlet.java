8be4d5031472fbf118733797727f99fd2d163b91
hilal.kasem24 <hilal.kasem24@gmail.com>
2022-05-22 09:24:08 -0400

    Add doctor blocking

--- src/main/java/com/health/HistoryInfoSystem/DoctorZoneServlet.java
+++ src/main/java/com/health/HistoryInfoSystem/DoctorZoneServlet.java
@@ -17,10 +17,14 @@ public class DoctorZoneServlet extends HttpServlet {
     }
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         Doctor doctor=controller.doctorLogin(request);
-        if(!Objects.isNull(doctor)){
+        if(!Objects.isNull(doctor) && !doctor.isBlocked()){
             HttpSession session=request.getSession();
             session.setAttribute("doctor",doctor);
             request.getRequestDispatcher("/doctorzone.jsp").forward(request,response);
+        }
+        else if(!Objects.isNull(doctor) && doctor.isBlocked()){
+            request.setAttribute("message","You're blocked. please contact your departments' admin.");
+            request.getRequestDispatcher("/doctorlogin.jsp").forward(request,response);
         }
         else {
             request.setAttribute("message","Username or Password is incorrect");
