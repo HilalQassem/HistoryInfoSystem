95e177c1ec6ffa7bea105700b5beaf21389ce5c3
hilal.kasem24 <hilal.kasem24@gmail.com>
2022-08-12 15:58:35 -0400

    UI improvements and populated DB

--- historyinfosystem.sql
+++ historyinfosystem.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 07, 2022 at 07:53 PM
+-- Generation Time: Aug 12, 2022 at 09:57 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.28
 
@@ -39,7 +39,7 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`adminId`, `name`, `username`, `password`) VALUES
-(2, 'Hilal Qassem', 'hilalqassem', '1653628292');
+(1, 'admin', 'admin', '92668751');
 
 -- --------------------------------------------------------
 
@@ -57,7 +57,11 @@ CREATE TABLE `department` (
 --
 
 INSERT INTO `department` (`departmentId`, `name`) VALUES
-(2, 'Emergency');
+(1, 'Emergency'),
+(2, 'Burn Unit'),
+(3, 'Cardiology'),
+(4, 'Critical Care'),
+(5, 'Diagnostic Imaging');
 
 -- --------------------------------------------------------
 
@@ -78,7 +82,11 @@ CREATE TABLE `dept_admin` (
 --
 
 INSERT INTO `dept_admin` (`adminId`, `name`, `departmentId`, `username`, `password`) VALUES
-(2, 'Hilal Qassem', 2, 'hilalqassem', '1653628292');
+(1, 'Rolland Jinks', 1, 'rollandjinks', '1611327677'),
+(2, 'Minta Eads', 2, 'mintaeads', '1122276074'),
+(3, 'Harold Sargent', 3, 'haroldsargent', '-1720200102'),
+(4, 'Angelina Morris', 4, 'angelinamorris', '895955975'),
+(5, 'Sebastian Daniel', 5, 'sebastiandaniel', '-569567357');
 
 -- --------------------------------------------------------
 
@@ -102,7 +110,16 @@ CREATE TABLE `doctor` (
 --
 
 INSERT INTO `doctor` (`doctorId`, `name`, `specialty`, `departmentId`, `username`, `password`, `isBlocked`, `isDeleted`) VALUES
-(1, 'Hilal Qassem', 'General', 2, 'hilalqassem', '1653628292', 0, 0);
+(1, 'Mikayla Akers', 'Disaster Medicine', 1, 'mikaylaakers', '-1230016512', 0, 0),
+(2, 'Selby Garfield', 'Medical Toxicology', 1, 'selbygarfield', '907014739', 0, 0),
+(3, 'Collin Webb', 'Burn Specialist', 2, 'collinwebb', '-1616551905', 0, 0),
+(4, 'Nora Scrivener', 'Burn Specialist', 2, 'norascrivener', '-1087188299', 0, 0),
+(7, 'Ulric Osbourne', 'Cardiac Electrophysiology', 3, 'ulricosbourne', '-1830894630', 0, 0),
+(8, 'Olive Tracy', 'Echocardiography', 3, 'olivetracy', '690159710', 0, 0),
+(9, 'Winona Sniders', 'Cardiovascular', 4, 'winonasniders', '-2085372630', 0, 0),
+(10, 'Cyrus Young', 'Respiratory', 4, 'cyrusyoung', '-950473730', 0, 0),
+(11, 'Gytha Rowbottom', 'Nuclear Radiology', 5, 'gytharowbottom', '2064950442', 0, 0),
+(12, 'Marjory Bellamy', 'Diagnostic Ultrasound', 5, 'marjorybellamy', '-1656600064', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -118,20 +135,6 @@ CREATE TABLE `patient` (
   `joinDate` date DEFAULT NULL,
   `ssn` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `patient`
---
-
-INSERT INTO `patient` (`patientId`, `name`, `dateOfBirth`, `number`, `joinDate`, `ssn`) VALUES
-(2, 'Hilal Qassem', '2000-01-24', '0938677316', '2020-01-01', '10260007552'),
-(3, 'Frank Joel', '1999-01-01', '099999999', '2022-05-15', '10260002145'),
-(4, 'HH', '2000-01-01', '55555', '2022-05-16', '555555'),
-(5, 'Hilal', '2000-05-05', '54646', '2022-05-16', '5645646'),
-(6, 'Frank', '2000-01-01', '645463464', '2022-05-16', '564654564'),
-(7, 'Hilal', '2022-06-23', '56298498', '2022-06-23', '10260002144'),
-(9, 'Hilal Qasse', '2022-06-23', '55555', '2022-06-23', '566561564'),
-(10, 'Frank Joel', '2020-07-06', '65156', '2022-06-23', '1584865121');
 
 -- --------------------------------------------------------
 
@@ -149,39 +152,6 @@ CREATE TABLE `visit` (
   `test` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `notes` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `visit`
---
-
-INSERT INTO `visit` (`visitId`, `doctorId`, `patientId`, `date`, `reason`, `prescription`, `test`, `notes`) VALUES
-(2, 1, 2, '2022-01-01', 'aaaa', 'aaaa', 'aaaa', ''),
-(7, 1, 3, '2021-02-02', '-', '-', '-', ''),
-(8, 1, 3, '2022-02-02', '--', '--', '--', ''),
-(10, 1, 4, '2021-01-01', '------------', '----------', '---------', ''),
-(11, 1, 5, '2021-05-06', 'dnsjhfqhji', 'bdsdbjhsd', 'jbdjwh', ''),
-(12, 1, 2, '2022-04-22', '------', '-------', '-------', 'dfjdsfnjkfn jfnierfer ijn '),
-(13, 1, 2, '2021-02-02', '-------', NULL, NULL, 'ffs'),
-(14, 1, 3, '2022-02-02', '+++-----', NULL, NULL, 'efewf'),
-(15, 1, 3, '2022-02-02', '+6+6+6+', NULL, NULL, 'fewff'),
-(16, 1, 3, '2022-02-02', 'sdsdfdsf', NULL, NULL, 'fsdfs'),
-(17, 1, 2, '2022-02-02', 'sdfsfs', NULL, NULL, 'dsffs'),
-(18, 1, 3, '2022-02-02', 'dwwe', NULL, NULL, ' dewfef'),
-(19, 1, 3, '2022-02-02', 'dewfer', NULL, NULL, 'sdffgdsg'),
-(20, 1, 3, '2022-05-04', 'dsfgd', NULL, NULL, 'fgdgfg'),
-(21, 1, 3, '2022-06-05', 'fdsfsgds', NULL, NULL, 'sdgfsgsdfg'),
-(22, 1, 3, '2022-02-22', 'dfggdfg', NULL, NULL, 'dsgs reg'),
-(23, 1, 2, '2022-02-22', 'dsfgrgrd', NULL, NULL, 'dsgddfgdg'),
-(24, 1, 3, '2022-02-22', 'dfbhdhb', NULL, NULL, 'dnbjhd'),
-(25, 1, 3, '2022-02-22', 'wefwfew', NULL, NULL, 'fdgggdfg'),
-(26, 1, 2, '2022-02-02', 'dgdfg', NULL, NULL, 'dsgdgr'),
-(27, 1, 3, '2022-02-02', 'dfgfdg', '', '', 'dfgdgfdg'),
-(28, 1, 3, '2022-02-02', 'fsdfsdf', NULL, NULL, 'dfgdfg'),
-(29, 1, 3, '2022-02-02', 'fdfg', NULL, NULL, 'gdfhd'),
-(30, 1, 2, '2022-02-02', 'dfssfsdf', 'c:\\upload\\prescription487493406.pdf', 'c:\\upload\\test422647911.pdf', 'fsdfsd'),
-(31, 1, 3, '2022-02-02', 'aasfddfs', 'prescription1009279118.pdf', 'test941093843.pdf', 'sfdff'),
-(32, 1, 4, '1985-02-24', 'efsfdgd', 'prescription108193950.pdf', 'test47398321.pdf', 'sdfsdgdrg'),
-(33, 1, 2, '2022-02-02', 'dssfg', 'prescription1477837635.pdf', NULL, 'dsfdsf');
 
 --
 -- Indexes for dumped tables
@@ -239,37 +209,37 @@ ALTER TABLE `visit`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `adminId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `adminId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `department`
 --
 ALTER TABLE `department`
-  MODIFY `departmentId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `departmentId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `dept_admin`
 --
 ALTER TABLE `dept_admin`
-  MODIFY `adminId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `adminId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `doctor`
 --
 ALTER TABLE `doctor`
-  MODIFY `doctorId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `doctorId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `patient`
 --
 ALTER TABLE `patient`
-  MODIFY `patientId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `patientId` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `visit`
 --
 ALTER TABLE `visit`
-  MODIFY `visitId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `visitId` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
--- src/main/webapp/doctorzone.jsp
+++ src/main/webapp/doctorzone.jsp
@@ -26,7 +26,7 @@
             <c:if test="${!empty message}">
             <div class="alert"><span class="closebtn" onclick="this.parentElement.style.display='none'">
             &times;</span><p>${message}</p></div>
-        </c:if>
+            </c:if>
         </form>
         <br/>
         <br/>
